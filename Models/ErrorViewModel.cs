dfc92edc197c546412a2e900e06e0acdcc27d115
Huuhoai2002 <huuhoai0511@gmail.com>
2022-10-06 07:41:23 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NghiemHuuHoaiBTH2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
